d5b9462d495752e08362656119a015aea2f51d43
Carla Marvin <carlamarvin78@gmail.com>
2019-08-09 15:21:30 -0400

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+const RegistrationForm = () => ( {errors, touched, values, handleSubmit, status}
+  const [user, setUser] = useState([]);
+  console.log(user);
+  <div>
+    <h1>REGISTER HERE</h1>
+  <Formik>
+
+
+)
+
+export default RegistrationForm
