55201ab809021d479877b629e18992ed7a40c7d2
Carla Marvin <carlamarvin78@gmail.com>
2019-08-09 23:18:03 -0400

    Form and validation working

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import './App.css';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { withFormik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from "yup";
 
-const RegistrationForm = () => ( {errors, touched, values, handleSubmit, status}
-  const [user, setUser] = useState([]);
-  console.log(user);
-  <div>
-    <h1>REGISTER HERE</h1>
-  <Formik>
+function RegistrationForm({errors, touched}) {
+  return (
+    <Form>
+      <div>
+        {touched.username && errors.username && <p>{errors.username}</p>}
+        <Field type="text" name="username" placeholder="Username" />
+      </div>
+      <div>
+        {touched.password && errors.password && <p>{errors.password}</p>}
+        <Field type="password" name="password" placeholder="Password" />
+      </div>
+
+      <button>Submit!</button>
+    </Form>
+  );
+}
+const FormikRegistrationForm = withFormik({
+
+  mapPropsToValues({ username, password }) {
+    return {
+      username: username || "",
+      password: password || ""
+    };
+  },
+//======VALIDATION SCHEMA==========
+validationSchema: Yup.object().shape({
+  username: Yup.string()
+    .min(4)
+    .required(),
+  password: Yup.string()
+    .min(8)
+    .required()
+}),
+//======END VALIDATION SCHEMA==========
+
+  handleSubmit(values) {
+  console.log(values);
+  //THIS IS WHERE YOU DO YOUR FORM SUBMISSION CODE... HTTP REQUESTS, ETC.
+  }
+})(RegistrationForm);
 
 
-)
 
-export default RegistrationForm
+
+export default FormikRegistrationForm
